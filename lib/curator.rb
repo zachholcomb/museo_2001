20b26ac434a2ca77e8a0fdd71700d35306bfdbb9
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-04 09:30:02 -0700

    Add add_artist test and add_photograph test to curator test and pass tests

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,2 +1,16 @@
 class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -7,70 +7,60 @@ require './lib/curator'
 class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
     assert_instance_of Curator, @curator
   end
+
+  def test_it_has_attributes
+    assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
+  end
+
+  def test_it_can_add_photographs
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+  def test_it_can_add_artists
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_it_can_find_artist_by_id
+
+  end
 end
 
 
-# pry(main)>
-# # => #<Curator:0x00007fb3d9111cc0...>
-#
-# pry(main)> curator.photographs
-# # => []
-#
-# pry(main)> photo_1 = Photograph.new({
-#      id: "1",
-#      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#      artist_id: "1",
-#      year: "1954"
-# })
-# # => #<Photograph:0x00007fb3d8ea1cd8...>
-#
-# pry(main)> photo_2 = Photograph.new({
-#      id: "2",
-#      name: "Moonrise, Hernandez",
-#      artist_id: "2",
-#      year: "1941"
-# })
-# # => #<Photograph:0x00007fb3d8e0bfa8...>
-#
-# pry(main)> curator.add_photograph(photo_1)
-#
-# pry(main)> curator.add_photograph(photo_2)
-#
-# pry(main)> curator.photographs
-# # => [#<Photograph:0x00007fb3d8ea1cd8...>, #<Photograph:0x00007fb3d8e0bfa8...>]
-#
-# pry(main)> curator.artists
-# # => []
-#
-# pry(main)> artist_1 = Artist.new({
-#     id: "1",
-#     name: "Henri Cartier-Bresson",
-#     born: "1908",
-#     died: "2004",
-#     country: "France"
-# })
-# # => #<Artist:0x00007fb3d8b4ed60...>
-#
-# pry(main)> artist_2 = Artist.new({
-#     id: "2",
-#     name: "Ansel Adams",
-#     born: "1902",
-#     died: "1984",
-#     country: "United States"
-# })
-# # => #<Artist:0x00007fb3d90bb4b0...>
-#
-# pry(main)> curator.add_artist(artist_1)
-#
-# pry(main)> curator.add_artist(artist_2)
-#
-# pry(main)> curator.artists
-# # => [#<Artist:0x00007fb3d8b4ed60...>, #<Artist:0x00007fb3d90bb4b0...>]
-#
+
 # pry(main)> curator.find_artist_by_id("1")
 # # => #<Artist:0x00007fb3d8b4ed60...>
