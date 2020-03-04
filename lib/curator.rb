ec244c9b525a72aee0dd73b143795621f347853e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-04 09:33:05 -0700

    Add find artist_by_id test to curator test and add method to pass test

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,4 +13,10 @@ class Curator
   def add_artist(artist)
     @artists << artist
   end
+
+  def find_artist_by_id(artist_id)
+    @artists.find do |artist|
+      artist.id == artist_id
+    end
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -56,11 +56,10 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_can_find_artist_by_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
 
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+    assert_equal @artist_2, @curator.find_artist_by_id("2")
   end
 end
-
-
-
-# pry(main)> curator.find_artist_by_id("1")
-# # => #<Artist:0x00007fb3d8b4ed60...>
